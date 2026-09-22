6e68bc23ececdead6a2d938731cf38a9587e3fbe
Syzafid <syafrizalaf93@gmail.com>
2026-09-22 14:24:29 +0700

    server

--- packages/client/src/socket.ts
+++ packages/client/src/socket.ts
@@ -1,7 +1,9 @@
 import { io, type Socket } from 'socket.io-client';
 import type { ServerToClientEvents, ClientToServerEvents } from '@werewolf/shared';
 
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io('/', {
+const serverUrl = import.meta.env.VITE_SERVER_URL || '/';
+
+export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(serverUrl, {
   autoConnect: true,
   reconnection: true,
   reconnectionAttempts: 10,
